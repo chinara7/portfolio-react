70e0ed7ad4a9871674fdd76dc782f1389d0e49c4
Chinara_Huseyn <chinare.huseynova7@gmail.com>
2022-01-12 13:16:29 +0400

    AboutMe section was created

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     <div className='container-fluid'>
       <Navbar />
       <Header />
+      <AboutMe />
     </div>
   );
 }
---
+++ src/component/AboutMe.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const AboutMe = () => {
+    return (
+        <div className='container py-5'>
+            <div className='row'>
+                <div className='col-lg-6 col-xm-12'>
+                    <div className='photo-wrap mb-5'>
+                        <img className='profile-img' src='https://source.unsplash.com/random' alt='' />
+                    </div>
+                </div>
+                <div className='col-lg-6 col-xm-12'>
+                    <h1 className='about-heading'>About me</h1>
+                    <p  className='p-text'>Culpa dolore laboris in ut. Mollit ut ullamco enim id sint laborum laboris enim eu ut dolor magna. Laboris deserunt adipisicing qui tempor fugiat fugiat nulla sunt irure ea amet duis. Nostrud Lorem labore do proident sint cupidatat minim dolore velit ex. Ut duis ipsum id aliquip aute ullamco id aliqua sit id eu anim. Non eu voluptate cillum amet ad.</p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default AboutMe
